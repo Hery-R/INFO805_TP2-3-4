24118470250971113b1fae79b771b99bb91b3bee
Maxime272003 <62075092+Maxime272003@users.noreply.github.com>
2025-02-07 09:30:29 +0100

    feat : ajout de la classe Arbre et mise à jour de l'analyseur syntaxique pour gérer les expressions

--- Exercice1/src/main/java/fr/usmb/m1isc/compilation/tp1/Arbre.java
+++ Exercice1/src/main/java/fr/usmb/m1isc/compilation/tp1/Arbre.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Arbre {
-    private String type;
-    private List<Object> enfants;
+    private final String type;
+    private final List<Object> enfants;
 
     public Arbre(String type) {
         this.type = type;
@@ -24,6 +24,50 @@ public class Arbre {
             sb.append(" ").append(enfant.toString());
         }
         sb.append(")");
+        return sb.toString();
+    }
+
+    public String generateCode() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("DATA SEGMENT\n");
+        for (Object enfant : enfants) {
+            if (enfant instanceof Arbre arbre) {
+                if (arbre.type.equals("LET")) {
+                    sb.append("\t").append(arbre.enfants.get(0)).append(" DD\n");
+                }
+            }
+        }
+        sb.append("DATA ENDS\n");
+
+        sb.append("CODE SEGMENT\n");
+        for (Object enfant : enfants) {
+            if (enfant instanceof Arbre arbre) {
+                switch (arbre.type) {
+                    case "LET" -> {
+                        sb.append("	mov eax, ").append(arbre.enfants.get(1)).append("\n");
+                        sb.append("	mov ").append(arbre.enfants.get(0)).append(", eax\n");
+                    }
+                    case "*" -> {
+                        sb.append("	mov eax, ").append(arbre.enfants.get(0)).append("\n");
+                        sb.append("	push eax\n");
+                        sb.append("	mov eax, ").append(arbre.enfants.get(1)).append("\n");
+                        sb.append("	pop ebx\n");
+                        sb.append("	mul eax, ebx\n");
+                    }
+                    case "/" -> {
+                        sb.append("	mov eax, ").append(arbre.enfants.get(0)).append("\n");
+                        sb.append("	push eax\n");
+                        sb.append("	mov eax, ").append(arbre.enfants.get(1)).append("\n");
+                        sb.append("	pop ebx\n");
+                        sb.append("	div ebx, eax\n");
+                    }
+                    default -> {
+                    }
+                }
+                            }
+        }
+        sb.append("CODE ENDS\n");
         return sb.toString();
     }
 }
--- Exercice1/src/main/java/fr/usmb/m1isc/compilation/tp1/Main.java
+++ Exercice1/src/main/java/fr/usmb/m1isc/compilation/tp1/Main.java
@@ -2,18 +2,25 @@ package fr.usmb.m1isc.compilation.tp1;
 
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception  {
-		 LexicalAnalyzer yy;
-		 if (args.length > 0)
-		        yy = new LexicalAnalyzer(new FileReader(args[0])) ;
-		    else
-		        yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
-		@SuppressWarnings("deprecation")
-		parser p = new parser (yy);
-		p.parse( );
-	}
+    public static void main(String[] args) throws Exception  {
+         LexicalAnalyzer yy;
+         if (args.length > 0)
+                yy = new LexicalAnalyzer(new FileReader(args[0])) ;
+            else
+                yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
+        @SuppressWarnings("deprecation")
+        parser p = new parser (yy);
+
+        @SuppressWarnings("unchecked")
+        List<Arbre> arbres = (List<Arbre>) p.parse().value;
+
+        for (Arbre arbre : arbres) {
+            System.out.println(arbre.generateCode());
+        }
+    }
 
 }
